032c6d8e235551339964bf335c0bcb718d04130a
Reece Martin <reece.martin@alumni.utoronto.ca>
2025-01-21 14:15:11 -0500

    Commiting my more compact addition function that runs with good runtime

--- 5-add-linked-list-numbers/add_linked_list_numbers.py
+++ 5-add-linked-list-numbers/add_linked_list_numbers.py
@@ -63,99 +63,39 @@ class SolutionListIntList:
         return reverse_linked_list(integer_to_linked_list(renum_1 + num_2))
     
 class SolutionListAddition:
-    def add_two_numbers_2(self, l1: ListNode, l2: ListNode) -> ListNode:
-        cur_node_1, cur_node_2 = l1, l2
+
+    def add_two_numbers_2_compact(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur_node_1, cur_node_2, prev_node_1, prev_node_2 = l1, l2, None, None
         carry_over = 0
-        while cur_node_1 != None and cur_node_2 != None:
-            # print(cur_node_1.val)
-            # print(cur_node_2.val)
-            value = cur_node_1.val + cur_node_2.val + carry_over
-            # print(value)
-            # print("\n")
 
-            if value > 10:
+        while cur_node_1 or cur_node_2:
+
+            if cur_node_1 and cur_node_2:
+                value = cur_node_1.val + cur_node_2.val + carry_over
+                cur_node_1.val = value % 10
                 carry_over = value // 10
-            elif value == 10:
-                carry_over = 1
-            else:
-                carry_over = 0
-
-            cur_node_1.val = value % 10
-            cur_node_1, cur_node_2 = cur_node_1.next, cur_node_2.next
-
-        # print("Middle")
-
-        if carry_over != 0 and cur_node_2 == None and cur_node_1 == None:
-            cur_node_1 = l1
-
-            while cur_node_1.next != None:
+                prev_node_1 = cur_node_1
                 cur_node_1 = cur_node_1.next
-
-            cur_node_1.next = ListNode(carry_over, None)
-
-        elif cur_node_1:
-
-            while cur_node_1:
-                print(carry_over)
-                print(cur_node_1.next)
-                if cur_node_1.next == None:
-                    value = (cur_node_1.val + carry_over)
-                    cur_node_1.val = value % 10
-                    cur_node_1.next = ListNode(value // 10, None)
-                    return l1
-                elif cur_node_1.val + carry_over > 9:
-                    print("Active")
-                    value = cur_node_1.val + carry_over
-                    cur_node_1.val = (value) % 10
-                    carry_over = (value) // 10
-                else:
-                    pass
-                # print("Rest of Item")
-                # print(cur_node_2.val + carry_over)
-                    carry_over = 0
+                prev_node_2 = cur_node_2
+                cur_node_2 = cur_node_2.next
+            elif cur_node_1:
+                value = cur_node_1.val + carry_over
+                cur_node_1.val = value % 10
+                carry_over = value // 10
+                prev_node_1 = cur_node_1
                 cur_node_1 = cur_node_1.next
-
-        elif cur_node_2:
-            cur_node_1, cur_node_2 = l1, l2
-
-            while cur_node_1.next != None and cur_node_2.next != None:
-                cur_node_1 = cur_node_1.next
+            else: # cur_node_2 exists
+                value = cur_node_2.val + carry_over
+                prev_node_1.next = ListNode(value % 10, None)
+                carry_over = value // 10
+                prev_node_1 = prev_node_1.next
+                prev_node_2 = cur_node_2
                 cur_node_2 = cur_node_2.next
 
-            while cur_node_2:
-                if cur_node_2.val + carry_over > 9:
-                    cur_node_1.next = ListNode((cur_node_2.val + carry_over) // 10)
-                    carry_over = carry_over % 10
-                else:
-                    cur_node_1.next = ListNode(cur_node_2.val + carry_over)
-                # print("Rest of Item")
-                # print(cur_node_2.val + carry_over)
-                    carry_over = 0
-                cur_node_1, cur_node_2 = cur_node_1.next, cur_node_2.next
+        if carry_over != 0:
+            prev_node_1.next = ListNode(carry_over, None)
 
-        def linked_list_to_integer(l: ListNode):
-            head = l
-            num = 0
-            digits = []
-
-            while l.next != None:
-                # print(digits)
-                # print(l.val)
-                # print(str(l.next.val) + "\n")
-                digits.append(l.val)
-                l = l.next
-            digits.append(l.val)
-            # print(digits)
-
-            for index, digit in enumerate(digits):
-                num += digit * (10 ** (len(digits) - index - 1))
-                # print(num)
-            
-            return num
-
-        print(465 + 742)
-        print(11 % 10)
-        print(linked_list_to_integer(l1))
         return l1
+
     
-SolutionListAddition.add_two_numbers_2(SolutionListAddition, ListNode(2, ListNode(4, ListNode(3, None))), ListNode(5, ListNode(6,ListNode(4, None))))
+SolutionListAddition.add_two_numbers_2_compact(SolutionListAddition, ListNode(2, ListNode(4, ListNode(3, None))), ListNode(5, ListNode(6,ListNode(4, None))))
