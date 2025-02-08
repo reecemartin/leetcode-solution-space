54cc7c231a51ba4ff5745568cd464eab3c700244
Reece Martin <reece.martin@alumni.utoronto.ca>
2025-02-08 14:38:45 -0500

    Breaking for today

--- 9-longest-no-repeat-substring/longest_unique_substring.py
+++ 9-longest-no-repeat-substring/longest_unique_substring.py
@@ -1,19 +1,41 @@
 def longest_no_repeat_substring(s):
-    longest_count = 0
-    current_count = 0
+    substrings = {}
+    max_size_set = 0
+    keys_to_remove = []
     
-    last_character = ''
-    for i in s:
-        if i == last_character:
-            print ("first active")
-            if current_count > longest_count:
-                print("second active")
-                longest_count = current_count
-            current_count = 1
+    # For every character in the string create a new substring in our dictionary which is the one starting from that index
+    for index, character in enumerate(s):
+        
+        substrings[index] = {""}
+        
+        # For all our substrings, if they don't have the current character add it, if they are shorter than the max length subset remove them
+        for x in substrings:
+            print("Inner Loop")
+            print(character)
+            print(substrings)
+            print(x)
+            print(character not in substrings[x])
+            
+            if character not in substrings[x]:
+                substrings[x].add(character)
+            else:
+                keys_to_remove.append(x)
+                
+        for key in keys_to_remove:
+            substrings.pop(key)
+        keys_to_remove.clear()
+        
+        print("Outer Loop \n")
+        print(substrings)
+                
+    print("Break")
+    print(substrings)
+    for x in substrings:
+        if len(substrings[x]) < max_size_set:
+            keys_to_remove.append(x)
         else:
-            print ("third active")
-            current_count += 1
-            last_character = i
-    return longest_count
+            max_size_set = len(substrings[x])
+            
+    return max_size_set - 1
 
 print(longest_no_repeat_substring("abcabcbb"))
