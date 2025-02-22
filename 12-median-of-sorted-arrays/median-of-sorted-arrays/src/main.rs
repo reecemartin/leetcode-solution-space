09c5cde31d478bfe37624c592a691fdeec0e9c4b
Reece Martin <reece.martin@alumni.utoronto.ca>
2025-02-21 22:35:14 -0500

    Getting closer . . .

--- 12-median-of-sorted-arrays/median-of-sorted-arrays/src/main.rs
+++ 12-median-of-sorted-arrays/median-of-sorted-arrays/src/main.rs
@@ -1,11 +1,12 @@
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
 
-        // Validated: Takes a vector and returns the index of the middle number of an odd length vector, or the left of middle in an even length vector
-        fn find_middle(nums: Vec<i32>) -> i32 {
-            if nums.len() % 2 == 0 {
-                (nums.len() / 2) as i32 - 1
+        // Validated: Takes a vector and returns the index of the middle number of an odd length vector, or the left of middle in an even length vector, starting
+        // at start and stopping before stop
+        fn find_middle(start: i32, stop: i32) -> i32 {
+            if (stop - start) % 2 == 0 {
+                ((stop - start) / 2) as i32 - 1 + start
             } else {
-                (nums.len() - 1) as i32 / 2
+                (stop - start - 1) as i32 / 2 + start
             }
         }
 
@@ -14,6 +15,29 @@ pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
         fn find_index( nums: Vec<i32>, target: i32) -> Vec<usize> {
             // Didn't want to implement binary search so I used code from here: https://spin.atomicobject.com/learning-rust-binary-search/
             // Add to my todo list to implement binary search
+
+            if nums.len() == 1 {
+                let correct_value = if nums[0] == target {1} else {0};
+                let index = if nums[0] > target {0} else {1};
+                return vec![index, correct_value];
+            }
+
+            if nums.len() == 2 {
+                if nums[0] == target {
+                    return vec![0 , 1];
+                }
+                if nums[1] == target {
+                    return vec![1 , 1];
+                }
+                if target < nums[0] {
+                    return vec![0, 0];
+                }
+                if target > nums[0] && target < nums[1] {
+                    return vec![1, 0];
+                }
+                return vec![2, 0];
+            }
+                
             let length = nums.len();
             let mut half = length / 2;
             let mut hind = length - 1;
@@ -28,7 +52,7 @@ pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
                     half = (hind + lind) / 2;
                     current = nums[half];
             }
-            return vec![lind - 1, 0];
+            return vec![lind, 0];
         }
 
         // Validated: Takes a vector, an index, and whether to consider that index in the count and returns a vector where the first number is the 
@@ -37,35 +61,78 @@ pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
             if in_nums {
                 vec![index, (nums.len() - (index + 1) as usize) as i32]
             } else {
-                vec![index + 1, (nums.len() - (index + 1) as usize) as i32]
+                vec![index, (nums.len() - (index) as usize) as i32]
             }      
         } 
 
+        let mut nums1_start = 0;
+        let mut nums2_start = 0;
+        let mut nums1_stop = nums1.len();
+        let mut nums2_stop = nums2.len();
 
-        let nums1_middle = find_middle(nums1.clone());
+        loop {
+            let nums1_middle_index = find_middle(nums1_start, nums1_stop as i32);
 
-        print!("Middle of Nums 1: {}\n", nums1[nums1_middle as usize]);
+            print!("Middle of Nums 1: {}\n", nums1[nums1_middle_index as usize]);
 
-        let loc_in_nums2 = find_index(nums2.clone(), nums1[nums1_middle as usize]);
+            let loc_in_nums2 = find_index(nums2.clone(), nums1[nums1_middle_index as usize]);
 
-        print!("{:?}\n", loc_in_nums2);
+            print!("Located in Nums2: {:?}\n", loc_in_nums2);
 
-        let g_l1 = find_greater_and_less_than(nums1.clone(), nums1_middle, true);
-        let g_l2 = find_greater_and_less_than(nums2, loc_in_nums2[0] as i32, loc_in_nums2[1] == 1);
-        print!("{:?}\n", g_l1);
-        print!("{:?}\n", g_l2);
-        if g_l1[0] + g_l2[0] - g_l1[1] + g_l2[1] >= 1 {
-            return nums1[nums1_middle as usize]
-        } 
-        
-        return -1
+            let g_l1 = find_greater_and_less_than(nums1.clone(), nums1_middle_index, true);
+            let g_l2 = find_greater_and_less_than(nums2.clone(), loc_in_nums2[0] as i32, loc_in_nums2[1] == 1);
+            print!("{:?}\n", g_l1);
+            print!("{:?}\n", g_l2);
+            print!("{:?}\n", g_l1[0] + g_l2[0] - (g_l1[1] + g_l2[1]));
+            if (g_l1[0] + g_l2[0] - (g_l1[1] + g_l2[1])).abs() <= 1 {
+                return nums1[nums1_middle_index as usize]
+            } 
+
+            let nums2_middle_index = find_middle(nums2_start, nums2_stop as i32);
+
+            print!("Middle of Nums 2: {}\n", nums2[nums2_middle_index as usize]);
+
+            let loc_in_nums1 = find_index(nums1.clone(), nums2[nums2_middle_index as usize]);
+
+            print!("Located in Nums1: {:?}\n", loc_in_nums1);
+
+            let g_l1 = find_greater_and_less_than(nums2.clone(), nums2_middle_index, true);
+            let g_l2 = find_greater_and_less_than(nums1.clone(), loc_in_nums1[0] as i32, loc_in_nums1[1] == 1);
+            print!("{:?}\n", g_l1);
+            print!("{:?}\n", g_l2);
+            if (g_l1[0] + g_l2[0] - (g_l1[1] + g_l2[1])).abs() <= 1 {
+                return nums2[nums2_middle_index as usize]
+            } 
+
+            print!("\n\n CHECK");
+            print!("{}\n",loc_in_nums2[0]);
+            print!("{}",nums2_middle_index as usize);
+            print!("\n\n");
+
+            if loc_in_nums2[0] - 1 < nums2[nums2_middle_index as usize] as usize {
+                nums1_start = nums1_middle_index;
+            } else {
+                nums1_stop = nums1_middle_index as usize;
+            }
+
+            if loc_in_nums1[0] - 1 < nums1[nums1_middle_index as usize] as usize {
+                nums2_start = nums2_middle_index;
+            } else {
+                nums2_stop = nums2_middle_index as usize;
+            }
+
+            print!("\n\n");
+            print!("{}\n",nums1_start);
+            print!("{}",nums1_stop);
+            print!("\n\n");
+        }
 
 
 }
 
 fn main() {
-    let mut vec1 = vec![1, 2, 3, 4, 5];
-    let mut vec2 = vec![1, 2, 4, 5];
+    let mut vec1 = vec![1, 2];
+    let mut vec2 = vec![3, 4];
 
     print!("Vector 1: {:?}\n", vec1);
     print!("Vector 2: {:?}\n", vec2);
